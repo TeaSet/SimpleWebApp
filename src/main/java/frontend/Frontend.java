61ad8c56f1565213972829235106abfd69da1de1
MazalTov <vorobiev.dima@yandex.ru>
2016-10-12 22:05:12 +0300

    commit

---
+++ src/main/java/frontend/Frontend.java
@@ -0,0 +1,47 @@
+package frontend;
+
+import templater.PageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Dmitriy on 12.10.2016.
+ */
+public class Frontend extends HttpServlet {
+    private String login = "";
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, Object> pageVariables = new HashMap<String, Object>();
+        pageVariables.put("lastLogin", login == null ? "" : login);
+
+        response.getWriter().println(PageGenerator.getPage("authform.html", pageVariables));
+
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        login = request.getParameter("login");
+
+        response.setContentType("text/html;charset=utf-8");
+
+        if (login == null ) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        }
+
+        else {
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+
+        Map<String, Object> pageVariables = new HashMap<String, Object>();
+        pageVariables.put("lastLogin", login == null ? "" : login);
+
+        response.getWriter().println(PageGenerator.getPage("authform.html", pageVariables));
+    }
+}
